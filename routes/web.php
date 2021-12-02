79b4df4fe42bb598449f75738e2bba468f9a487b
Stanley Tsonev <stanleytsonev@hotmail.com>
2021-12-02 08:07:23 -0500

    Added Models and Factories for Clients and Notifs

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostsController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -16,9 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('test');
-// });
+Route::get('/', function (Request $request) {
+    return view('test', ['request' => $request]);
+});
 
 Route::get('/posts', [PostsController::class, 'index']);
 
