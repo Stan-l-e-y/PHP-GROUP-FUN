509062a24fc4f4512c44d8cfef427b4080e3c590
Stanley Tsonev <stanleytsonev@hotmail.com>
2021-12-09 22:14:36 -0500

    clientside verification on prof pic removed requir

--- resources/views/user-create.blade.php
+++ resources/views/user-create.blade.php
@@ -53,7 +53,7 @@
 
                 <div class="mb-6">
                   <label for="picture" class="block mb-2 uppercase font-bold text-xs text-gray-700">Profile Picture</label>
-                  <input type="file" class="border border-gray-400 p-2 w-full" name="picture" id="picture"  required>
+                  <input type="file" class="border border-gray-400 p-2 w-full" name="picture" id="picture"  >
                   @error('picture')
                     <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
                   @enderror
